930e451f545c42034008e04f2df65aae20401150
Antonina Buchak <antoninabucak@gmail.com>
2022-10-23 10:21:51 +0300

    task_2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,25 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("ul#ingredients");
+
+const arr = [];
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  console.log(ingredients[i]);
+
+  const ingredient = document.createElement("li");
+  ingredient.textContent = ingredients[i];
+  console.log(ingredient);
+
+  ingredient.classList.add("item");
+  console.log(ingredient.classList);
+  arr.push(ingredient);
+}
+list.append(...arr);
