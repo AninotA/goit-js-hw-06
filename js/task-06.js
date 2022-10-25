2ce10af89c8ee1a502c2ce510fe8790ec569865b
Antonina Buchak <antoninabucak@gmail.com>
2022-10-25 10:20:57 +0300

    Update task_6

--- js/task-06.js
+++ js/task-06.js
@@ -5,8 +5,9 @@ const input = document.querySelector("#validation-input");
 // function onInputChange(event) {
 
 //   if (event.currentTarget.value.length == input.dataset.length) {
+//     input.classList.remove("invalid");
 //     input.classList.add("valid");
-//     // console.log(input.classList)
+  
 //   }
 //    else {
 //     input.classList.remove("valid");
@@ -17,19 +18,12 @@ const input = document.querySelector("#validation-input");
 // }
 
 
-// input.addEventListener("blur", () => {
-//   if (input.value.length == input.dataset.length) {
-//     return input.classList.add("valid");
-//   }
-//   return input.classList.add("invalid");
-// });
-
-
 
 input.addEventListener("blur", onValidInput);
 
 function onValidInput() {
   if (input.value.length === Number(input.dataset.length)) {
+    input.classList.remove("invalid");
     input.classList.add("valid");
   } else {
     input.classList.remove("valid");
@@ -37,5 +31,5 @@ function onValidInput() {
   }
 
   console.log(event.currentTarget.value.length);
-//   console.log(input.dataset.length);
+
 }
