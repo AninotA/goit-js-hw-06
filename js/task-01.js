c5ea8091955568ca17044a641d1c5e2a725e1737
Antonina Buchak <antoninabucak@gmail.com>
2022-10-28 18:50:17 +0300

    Update task_1

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,25 @@
 const navElement = document.querySelector('#categories');
-const listItem = navElement.querySelectorAll('.item');
+const item = navElement.querySelectorAll('.item');
 
-console.log(`Number of categories: ${listItem.length}`);
+console.log(`Number of categories: ${item.length}`);
 // console.log("");
 
 
+Array.prototype.forEach.call(item, (element) => {
+    const title = element.querySelector('h2').innerHTML;
+    const itemsLength = element.querySelectorAll('li').length;
+    console.log(`Category: ${title}`);
+    console.log(`Elements: ${itemsLength}`);
+});
 
-for (let i = 0; i < listItem.length; i++) {
-    console.log("Category: " + listItem[i].querySelector('h2').textContent);
-    console.log("Elements: " + listItem[i].querySelectorAll('ul li').length);
-    console.log("");
-}
+    // console.log("Category:", element.querySelector('h2').textContent);
+    // console.log("Elements:", element.querySelectorAll('ul li').length);
+
+
+
+// for (let i = 0; i < item.length; i++) {
+//     console.log("Category: " + listItem[i].querySelector('h2').textContent);
+//     console.log("Elements: " + listItem[i].querySelectorAll('ul li').length);
+//     console.log("");
+// }
+
