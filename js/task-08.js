e29f1078d78cc39755d032babe6b39988b03c720
Antonina Buchak <antoninabucak@gmail.com>
2022-10-30 12:18:41 +0200

    Update

--- js/task-08.js
+++ js/task-08.js
@@ -1,22 +1,29 @@
 const form = document.querySelector(".login-form");
 
+// const formLogin = {};
+
 const email = form[0];
 const password = form[1];
 
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
   event.preventDefault();
 
   if (email.value === "" || password.value === "") {
     alert(`всі поля повинні бути заповнені`);
   } else {
-    const elements = event.currentTarget;
-    const mailValue = elements.email.value;
-    const passwordValue = elements.password.value;
+    const formElements = event.currentTarget;
+    const mailValue = formElements.email.value;
+    const passwordValue = formElements.password.value;
+
+    console.log(formElements);
     console.log("Mail:", mailValue);
     console.log("Password:", passwordValue);
   }
-  console.log(event);
-  return form.reset();
-  // console.log(`email: ${email.value}, password: ${password.value}`);
-});
+  form.reset();
+}
 
+// console.log(event);
+//   return form.reset();
+//   // console.log(`email: ${email.value}, password: ${password.value}`);
